dd1f99b15b028c91d244089c367850b06c0ac7dd
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-01-19 02:29:56 -0500

    Assets

--- Empires Of The IV/EmpiresOfTheIV/EmpiresOfTheIV.Shared/Game/LoadingManager.cs
+++ Empires Of The IV/EmpiresOfTheIV/EmpiresOfTheIV.Shared/Game/LoadingManager.cs
@@ -56,32 +56,41 @@ namespace EmpiresOfTheIV.Game
             // Load the Assets
             m_game.ResourceManager.LoadAsset(m_game.Content, typeof(Texture2D), "KillerrinStudiosLogo");
             m_game.ResourceManager.LoadAsset(m_game.Content, typeof(AnimatedModel), "t-pose_3");
-
+            m_game.ResourceManager.LoadAsset(m_game.Content, typeof(Model), "Tyril");
+            
             // Load the Terrain
-            Texture2D heightMap = m_game.Content.Load<Texture2D>("heightmap");
-            Texture2D grassTexture = m_game.Content.Load<Texture2D>("grassTexture");
-            Terrain m_terrain = new Terrain(graphics, heightMap, grassTexture);
-            m_game.SceneManager.CurrentScene.SceneNode.AddChild(m_terrain.Transform);
-
+            //Texture2D heightMap = m_game.Content.Load<Texture2D>("heightmap");
+            //Texture2D grassTexture = m_game.Content.Load<Texture2D>("grassTexture");
+            //Terrain m_terrain = new Terrain(graphics, heightMap, grassTexture);
+            //m_game.SceneManager.CurrentScene.SceneNode.AddChild(m_terrain.Transform);
+            
             // Create the Game Objects
             Unit armyGuy = new Unit();
             armyGuy.Model3D = m_game.ResourceManager.GetAsset(typeof(AnimatedModel), "t-pose_3") as AnimatedModel;
             armyGuy.Transform.Scale = new Vector3(0.007f);
-            armyGuy.Transform.Position = new Vector3(0.2f, -0.5f, 0.50f);
+            armyGuy.Transform.Position = new Vector3(0.2f, -0.5f, -5.50f);
+
+            Planet tyril = new Planet();
+            tyril.Model3D = m_game.ResourceManager.GetAsset(typeof(Model), "Tyril") as Model;
+            tyril.Transform.Scale = new Vector3(100.0f);
+            tyril.Transform.Position = new Vector3(0.0f, 0.0f, -10.0f);
 
             // Add to the Scene
             m_game.SceneManager.CurrentScene.SceneNode.AddChild(armyGuy.Transform);
+            m_game.SceneManager.CurrentScene.SceneNode.AddChild(tyril.Transform);
+            
+            //// Load the models which contain animations
+            //AnimatedModel walk = CustomContentLoader.LoadAnimatedModel(m_game.Content, "walk");
+            //AnimationClip clip = walk.Clips[0];
+            //
+            //// Set our animations to the gameObjects
+            //AnimationPlayer armyGuyPlayer = armyGuy.PlayClip(clip);
+            //armyGuyPlayer.Looping = true;
 
-            // Load the models which contain animations
-            AnimatedModel walk = CustomContentLoader.LoadAnimatedModel(m_game.Content, "walk");
-            AnimationClip clip = walk.Clips[0];
-
-            // Set our animations to the gameObjects
-            AnimationPlayer armyGuyPlayer = armyGuy.PlayClip(clip);
-            armyGuyPlayer.Looping = true;
 
 
-            m_game.SceneManager.CurrentScene.Camera.MoveVertical(30.0f);
+            
+            //m_game.SceneManager.CurrentScene.Camera.MoveVertical(30.0f);
         }
     }
 }
