9f907884ffbdd5141b1a5658cb0ab357c81a7da5
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-01-25 19:44:29 -0500

    Updating DLLs

--- Empires Of The IV/EmpiresOfTheIV/EmpiresOfTheIV.Shared/Consts.cs
+++ Empires Of The IV/EmpiresOfTheIV/EmpiresOfTheIV.Shared/Consts.cs
@@ -7,5 +7,7 @@ namespace EmpiresOfTheIV
     public static class Consts
     {
         public static EmpiresOfTheIVGame Game;
+
+        public static readonly Guid BluetoothGUID = new Guid("20BB9225-AC4B-4ACD-9B12-126D8EBCF2D6");
     }
 }
